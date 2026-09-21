601f43207b364321c01dfb2965c735fb7544b4d2
Ayaan Saifi <ayaansaifi2005@gmail.com>
2026-09-21 22:53:58 +0530

    feat: add secure zero-leak superuser auto-provisioning to build.sh

--- bluebug-backend/build.sh
+++ bluebug-backend/build.sh
@@ -7,3 +7,27 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically ensure superuser exists if env vars are present (zero credentials hardcoded)
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@bluebug.xyz')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser {username} created.')
+else:
+    u = User.objects.get(username=username)
+    u.set_password(password)
+    u.is_staff = True
+    u.is_superuser = True
+    u.save()
+    print(f'Superuser {username} synced.')
+"
+fi
